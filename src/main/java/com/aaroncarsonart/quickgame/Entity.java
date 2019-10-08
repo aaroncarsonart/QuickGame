8ee960e696c2ec7a6c08f6efd362d42bee4513cc
Aaron Carson <acarson13@mail.wou.edu>
2019-10-08 12:49:46 -0600

    Create basic @ walking on a map

---
+++ src/main/java/com/aaroncarsonart/quickgame/Entity.java
@@ -0,0 +1,7 @@
+package com.aaroncarsonart.quickgame;
+
+import imbroglio.Position2D;
+
+public class Entity {
+    private Position2D position2D;
+}
---
+++ src/main/java/com/aaroncarsonart/quickgame/Main.java
@@ -0,0 +1,11 @@
+package com.aaroncarsonart.quickgame;
+
+import com.aaroncarsonart.quickgame.GameWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, QuickGame!");
+        GameWindow gameWindow = new GameWindow();
+        gameWindow.startGame();
+    }
+}
